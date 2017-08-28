aec915d5245cd83f334618846506f544a691949a
GHTYQ <wy_tianyanqing@163.com>
2017-08-28 15:14:19 +0800

    Initial Commit

---
+++ ApplePayDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ApplePayDemo
+//
+//  Created by Pan on 2017/8/28.
+//  Copyright © 2017年 Pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ApplePayDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ApplePayDemo
+//
+//  Created by Pan on 2017/8/28.
+//  Copyright © 2017年 Pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
