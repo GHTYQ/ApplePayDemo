64a776dfc458a6a6b9a645f85f61fadbda0a78d8
GHTYQ <wy_tianyanqing@163.com>
2017-08-28 16:15:31 +0800

    haha

--- ApplePayDemo/ViewController.h
+++ ApplePayDemo/ViewController.h
@@ -7,8 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
-
-@interface ViewController : UIViewController
+#import <StoreKit/StoreKit.h>
+@interface ViewController : UIViewController<SKPaymentTransactionObserver,SKProductsRequestDelegate>
 
 
 @end
--- ApplePayDemo/ViewController.m
+++ ApplePayDemo/ViewController.m
@@ -4,12 +4,21 @@
 //
 //  Created by Pan on 2017/8/28.
 //  Copyright © 2017年 Pan. All rights reserved.
-//
-
+/**
+ 支付说明：
+ 一：想appstore提交资料填写协议
+ 二：创建内购项目
+ 三：注册沙箱账号
+ */
 #import "ViewController.h"
-
+//沙盒测试环境验证
+#define SANDBOX @"https://sandbox.itunes.apple.com/verifyReceipt"
+//正式环境验证
+#define AppStore @"https://buy.itunes.apple.com/verifyReceipt"
+//98元 xxxx代表bundleID
+#define Money98 @"xxxxxxxx.98"
 @interface ViewController ()
-
+@property(nonatomic,strong)NSString  *productID;
 @end
 
 @implementation ViewController
@@ -18,6 +27,175 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     self.view.backgroundColor=[UIColor redColor];
+}
+
+/**
+ 苹果支付的点击事件
+ */
+- (IBAction)applePayBtnClicked:(id)sender {
+    [[SKPaymentQueue defaultQueue] addTransactionObserver:self];
+   
+        self.productID = Money98;
+        if([SKPaymentQueue canMakePayments]){
+            [self requestProductData:self.productID];
+        }else{
+            NSLog(@"不允许程序内付费");
+            
+            UIAlertView *alerView = [[UIAlertView alloc] initWithTitle:@"提示" message:@"您的手机没有打开程序内付费购买"
+                                                              delegate:nil cancelButtonTitle:NSLocalizedString(@"关闭",nil) otherButtonTitles:nil];
+            [alerView show];
+            
+        }
+    
+}
+
+//请求商品
+- (void)requestProductData:(NSString *)type{
+    NSLog(@"-------------请求对应的产品信息----------------");
+//    [SVProgressHUD show];
+    NSArray *product = [[NSArray alloc] initWithObjects:type, nil];
+    NSSet *nsset = [NSSet setWithArray:product];
+    SKProductsRequest *request = [[SKProductsRequest alloc] initWithProductIdentifiers:nsset];
+    request.delegate = self;
+    [request start];
+    
+}
+//收到产品返回信息
+- (void)productsRequest:(SKProductsRequest *)request didReceiveResponse:(SKProductsResponse *)response{
+    
+    NSLog(@"--------------收到产品反馈消息---------------------");
+    
+    NSArray *product = response.products;
+    if([product count] == 0){
+        NSLog(@"--------------没有商品------------------");
+        return;
+    }
+    
+    NSLog(@"productID:%@", response.invalidProductIdentifiers);
+    NSLog(@"产品付费数量:%lu",(unsigned long)[product count]);
+    
+    SKProduct *p = nil;
+    for (SKProduct *pro in product) {
+        NSLog(@"%@", [pro description]);
+        NSLog(@"%@", [pro localizedTitle]);
+        NSLog(@"%@", [pro localizedDescription]);
+        NSLog(@"%@", [pro price]);
+        NSLog(@"%@", [pro productIdentifier]);
+        //        如果后台消费条目的ID与我这里需要请求的一样（用于确保订单的正确性）
+        if([pro.productIdentifier isEqualToString:self.productID]){
+            p = pro;
+        }
+    }
+    
+    SKPayment *payment = [SKPayment paymentWithProduct:p];
+    
+    NSLog(@"发送购买请求");
+    [[SKPaymentQueue defaultQueue] addPayment:payment];
+}
+
+//请求失败
+- (void)request:(SKRequest *)request didFailWithError:(NSError *)error{
+    NSLog(@"------------------错误-----------------:%@", error);
+//    [VZ_alertView showMessage:error.localizedDescription];
+}
+
+- (void)requestDidFinish:(SKRequest *)request{
+    NSLog(@"------------反馈信息结束-----------------");
+//    [SVProgressHUD dismiss];
+}
+//监听购买结果
+- (void)paymentQueue:(SKPaymentQueue *)queue updatedTransactions:(NSArray *)transaction{
+    for(SKPaymentTransaction *tran in transaction){
+        
+        switch (tran.transactionState) {
+            case SKPaymentTransactionStatePurchased:
+                NSLog(@"交易完成");
+                [self completeTransaction:tran];
+                break;
+            case SKPaymentTransactionStatePurchasing:
+                NSLog(@"商品添加进列表");
+                
+                break;
+            case SKPaymentTransactionStateRestored:
+                NSLog(@"已经购买过商品");
+                [self restoreTransaction:tran];
+                break;
+            case SKPaymentTransactionStateFailed:
+                NSLog(@"交易失败");
+                [self failedTransaction:tran];
+                
+                break;
+            default:
+                break;
+        }
+    }
+}
+// 交易失败
+- (void)failedTransaction:(SKPaymentTransaction *)transaction {
+    if(transaction.error.code != SKErrorPaymentCancelled) {
+        NSLog(@"购买失败");
+//        [VZ_alertView showMessage:@"购买失败，请重新购买"];
+    } else {
+        NSLog(@"用户取消交易");
+    }
+    [[SKPaymentQueue defaultQueue] finishTransaction: transaction];
+}
+// 已购商品
+- (void)restoreTransaction:(SKPaymentTransaction *)transaction {
+    // 对于已购商品，处理恢复购买的逻辑
+    [[SKPaymentQueue defaultQueue] finishTransaction: transaction];
+}
+//交易结束
+- (void)completeTransaction:(SKPaymentTransaction *)transaction{
+    NSLog(@"交易结束");
+    //交易验证
+    NSURL *recepitURL = [[NSBundle mainBundle] appStoreReceiptURL];
+    NSData *receipt = [NSData dataWithContentsOfURL:recepitURL];
+    if(!receipt){
+        
+    }
+    NSError *error;
+    NSDictionary *requestContents = @{
+                                      @"receipt-data": [receipt base64EncodedStringWithOptions:0]
+                                      };
+    NSData *requestData = [NSJSONSerialization dataWithJSONObject:requestContents
+                                                          options:0
+                                                            error:&error];
+    
+    if (!requestData) { /* ... Handle error ... */ }
+    //In the test environment, use https://sandbox.itunes.apple.com/verifyReceipt
+    //In the real environment, use https://buy.itunes.apple.com/verifyReceipt
+    // Create a POST request with the receipt data.
+    NSURL *storeURL = [NSURL URLWithString:SANDBOX];
+    NSMutableURLRequest *storeRequest = [NSMutableURLRequest requestWithURL:storeURL];
+    [storeRequest setHTTPMethod:@"POST"];
+    [storeRequest setHTTPBody:requestData];
+    
+    // Make a connection to the iTunes Store on a background queue.
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    [NSURLConnection sendAsynchronousRequest:storeRequest queue:queue
+                           completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+                               if (connectionError) {
+                                   /* ... Handle error ... */
+                               } else {
+                                   NSError *error;
+                                   NSDictionary *jsonResponse = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+                                   if (!jsonResponse) { /* ... Handle error ...*/ }
+                                   /* ... Send a response back to the device ... */
+                                   //Parse the Response
+                                   NSLog(@"jsonResponse===%@",jsonResponse);
+                                  //将支付信息传给后台服务器
+                               }
+                           }];
+    
+    [[SKPaymentQueue defaultQueue] finishTransaction:transaction];
+}
+
+
+
+- (void)dealloc{
+    [[SKPaymentQueue defaultQueue] removeTransactionObserver:self];
+    //    [super dealloc];
 }
 
 
